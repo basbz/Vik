3e34125c77551787fbd4b3908bc805e0c114c1cd
Bas Ben Zineb <bas.benzineb@gmail.com>
2013-02-15 14:39:52 +0100

    rendering canvas, porting to SVG with bash/php

--- process.js
+++ process.js
@@ -3,16 +3,14 @@ $(document).ready(function () {
       $buffer = $('#buffer'), buffer = $buffer[0],
       $canvas = $('#canvas'), canvas = $canvas[0],
       img = new Image(),
-      src,
+      src, paper,
       bctx = buffer.getContext('2d'),
       cctx = canvas.getContext('2d'),
+      step = Number(window.location.hash.split('=')[1]) || 4,
       imgs = ['kings.gif'];
 
-  for(var i = 0; i < 1000; i++) {
-    if(Math.floor(Math.random() * 6)) {
-      xx.push(i);
-    }
-  }
+  console.log(step);
+  
 
   function grayscale (color) {
     var gr = (Math.round(color[0] * 0.21) + Math.round(color[1] * 0.71) + Math.round(color[2] * 0.07));
@@ -41,22 +39,34 @@ $(document).ready(function () {
   }
 
   function imgLoad () {
-    var j = 0;
+    var j = 0, w = img.width, h = img.height;
 
-    bctx.drawImage(img, 0, 0, img.width, img.height);
+    for(var i = 0; i < w; i++) {
+      if(Math.floor(Math.random() * 9)) {
+        xx.push(i);
+      }
+    }
+    $buffer.attr({width: w, height: h});
+    bctx.drawImage(img, 0, 0, w, h);
+
+    h *= step
+    w *= step
+
+    $canvas.attr({width: w, height: h});
 
     loop = setInterval(function () {
       var x = 0, l = xx.length, i, p, rbga;
-      if(j < img.height) {
-        for(; x < l; x += 3) {
+      if(j < h) {
+        for(; x < l; x += step) {
           i = xx[x];
-          if(i < img.width) {
-             cctx.fillStyle = grayscale(bctx.getImageData(i, j, 3, 3).data);
+          if(i < w) {
+             cctx.fillStyle = grayscale(bctx.getImageData(i, j, step, step).data);
              unit = 2 + Math.floor(Math.random() * 38);
-             cctx.fillRect(i * 3 , j * 3, 1, unit);
+             cctx.fillRect(i * step , j * step, 1, unit);
           } 
         }
-        j += 3;
+        j += step;
+        console.log('loop: ' + j);
       } else {
         clearInterval(loop);
         console.log('parsed');
@@ -70,42 +80,5 @@ $(document).ready(function () {
 
   img.onload = imgLoad;
   img.src = imgs.shift();
+  
 });
-//var i, x = 0, l = xx.length - 1, j = 0, p, gr, unit, hex;
-
-
-  //for(; x < l; x += 1) {
-  //i = xx[x];
-  //if(i < img.width) {
-  //for(j = 0; j < img.height; j += 2) {
-  //p = ctx.getImageData(i, j, 2, 2).data;
-  //gr = ((p[0] * .21) + (p[1] * .71) + (p[2] * .07));
-  //hex = "#fff";
-////if(gr < 130) {
-//// hex = "#82d0f5";
-////} 
-
-//if(gr < 120) {
-//hex = "#009fe3";
-//} 
-
-//unit = 4 + Math.floor(Math.random() * 36);
-//if(hex !== '#fff') {
-//rect = paper.rect(yOff + (i * 6) , j * 6 , 2, unit);
-//rect.attr({
-//fill: hex,
-//'stroke-opacity': 0
-//});
-//}
-//}
-//}
-//}
-
-//if(imgs.length) {
-//yOff += 120;
-//img.src = imgs.shift();
-//} else {
-//canvas.parentNode.removeChild(canvas);
-//console.log(paper.toSVG());
-//} 
-
